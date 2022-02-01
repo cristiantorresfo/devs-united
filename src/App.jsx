8cb13bc4ac5477ed5b11f0e94b33fea9cbb144e6
cristiantorresfo <cristorresf@outlook.com>
2022-01-31 19:01:22 -0500

    Componetización y creacion de pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,34 @@
+import { useState } from "react";
 import "./App.css";
-import { useEffect, useState } from "react";
-import { collection, onSnapshot } from "firebase/firestore";
-import { db, auth } from "./firebase";
 import Posts from "./components/Posts";
-import LogIn from "./components/LogIn";
+import TypingField from "./components/TypingField";
+import Feed from "./pages/Feed";
+// import { useEffect, useState } from "react";
+// import { collection, onSnapshot } from "firebase/firestore";
+// import { db, auth } from "./firebase";
+// import Posts from "./components/Posts";
+// import LogIn from "./components/LogIn";
+import LoggedOut from "./pages/LoggedOut";
+import Welcome from "./pages/Welcome";
 
-const INITIAL_FORM_DATA = {
-  message: "",
-  email: "",
-  uid: "",
-  autor: "",
-};
+
+
 const USER_INITIAL = {
   uid: "",
 };
-
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [newPost, setNewPost] = useState(INITIAL_FORM_DATA);
+  
   const [userLog, setUserLog] = useState(USER_INITIAL);
 
-  useEffect(() => {
-    const unsub = onSnapshot(collection(db, "posts"), (snapshot) => {
-      const postsData = snapshot.docs.map(
-        (doc) => {
-          return {
-            message: doc.data().message,
-            id: doc.id,
-            likes: doc.data().likes,
-            autor: doc.data().autor,
-            email: doc.data().email,
-            uid: doc.data().uid,
-          };
-        },
-        (error) => {
-          console.log(error, "error de escucha");
-        }
-      );
-      setPosts(postsData);
-    });
-    const unsubscribeAuth = auth.onAuthStateChanged((user) => {
-      setUserLog(user || USER_INITIAL);
-    });
-    return () => {
-      unsub();
-      unsubscribeAuth();
-    };
-  }, []);
 
   return (
     <div className="App">
-      <LogIn userLog={userLog} />
+      <LoggedOut/>
+      <Welcome/>
+      <Feed userLog={userLog} setUserLog={setUserLog} USER_INITIAL={USER_INITIAL}/>
+      
+
+      {/* <LogIn userLog={userLog} />
       {userLog.uid.length !== 0 ? (
         <Posts
           posts={posts}
@@ -60,7 +38,7 @@ function App() {
           setNewPost={setNewPost}
           INITIAL_FORM_DATA={INITIAL_FORM_DATA}
         />
-      ) : null}
+      ) : null} */}
     </div>
   );
 }
